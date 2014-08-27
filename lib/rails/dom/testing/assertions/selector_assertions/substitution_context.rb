2f4b29c15d7c0d24f7baa6f502b327ca3c30d83e
Kasper Timm Hansen <kaspth@gmail.com>
2014-08-27 10:29:55 +0200

    Refactor SubstitutionContext
    - Reorder methods so public interface is more clear
    - Make methods private
    - Don't allow substitute to be changed

--- lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
+++ lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
@@ -1,28 +1,31 @@
 class SubstitutionContext
-  def initialize(substitute = '?')
-    @substitute = substitute
+  def initialize
+    @substitute = '?'
     @regexes = []
   end
 
-  def add_regex(regex)
-    # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
-    return regex.inspect unless regex.is_a?(Regexp)
-    @regexes.push(regex)
-    last_id.to_s # avoid implicit conversions of Fixnum to String
-  end
-
-  def last_id
-    @regexes.count - 1
+  def substitute!(selector, values)
+    while !values.empty? && selector.index(@substitute)
+      selector.sub! @substitute, add_regex(values.shift)
+    end
+    selector
   end
 
   def match(matches, attribute, id)
     matches.find_all { |node| node[attribute] =~ @regexes[id] }
   end
 
-  def substitute!(selector, values)
-    while !values.empty? && selector.index(@substitute)
-      selector.sub!(@substitute, add_regex(values.shift))
+  private
+    def add_regex(regex)
+      if regex.is_a?(Regexp)
+        @regexes.push(regex)
+        last_id.to_s # avoid implicit conversions of Fixnum to String
+      else
+        regex.inspect # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
+      end
     end
-    selector
-  end
+
+    def last_id
+      @regexes.count - 1
+    end
 end
