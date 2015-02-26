f2a7eb412c5f86270d4d04471c38f10c3d369fb6
Kasper Timm Hansen <kaspth@gmail.com>
2015-02-26 22:21:11 +0100

    Remove substitution_ids and interpret every matcher as a Regexp.

--- lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
+++ lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
@@ -1,27 +1,21 @@
 class SubstitutionContext
   def initialize
     @substitute = '?'
-    @regexes = []
   end
 
   def substitute!(selector, values)
     while !values.empty? && substitutable?(values.first) && selector.index(@substitute)
-      selector.sub! @substitute, substitution_id_for(values.shift)
+      selector.sub! @substitute, matcher_for(values.shift)
     end
   end
 
-  def match(matches, attribute, substitution_id)
-    matches.find_all { |node| node[attribute] =~ @regexes[substitution_id] }
+  def match(matches, attribute, matcher)
+    matches.find_all { |node| node[attribute] =~ Regexp.new(matcher) }
   end
 
   private
-    def substitution_id_for(value)
-      if value.is_a?(Regexp)
-        @regexes << value
-        @regexes.size - 1
-      else
-        value
-      end.inspect # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
+    def matcher_for(value)
+      value.to_s.inspect # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
     end
 
     def substitutable?(value)
