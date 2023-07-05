1c306c5cfa1c71dc8e53322184539a4038b7fe60
Mike Dalessio <mike.dalessio@gmail.com>
2023-07-05 16:55:52 -0400

    feat: Introduce Rails::Dom::Testing.default_html_version
    Also provide helper methods:
    
    - `Rails::Dom::Testing.html_document`
    - `Rails::Dom::Testing.html_document_fragment`
    
    which will return a parser class based on the default or an optional
    injected `html_version:` kwarg.
    
    These methods will raise an exception if the configuration cannot be
    honored (e.g., the HTML5 parser isn't supported on the platform).

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,15 @@ require "active_support/test_case"
 require "minitest/autorun"
 
 ActiveSupport::TestCase.test_order = :random
+
+module DomTestingHelpers
+  def with_default_html_version(version)
+    old_version = Rails::Dom::Testing.default_html_version
+    begin
+      Rails::Dom::Testing.default_html_version = version
+      yield
+    ensure
+      Rails::Dom::Testing.default_html_version = old_version
+    end
+  end
+end
