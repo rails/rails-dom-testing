f8b8e051e55a58e2c8ea35233749fa7bcd8f7e2a
Rafael Mendonça França <rafael@rubyonrails.org>
2023-08-03 14:21:25 +0000

    Remove constants from global namespace
    They are all private of this library so let's just define then in the
    library namespace.

--- lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
+++ lib/rails/dom/testing/assertions/selector_assertions/substitution_context.rb
@@ -1,32 +1,42 @@
 # frozen_string_literal: true
 
-class SubstitutionContext
-  def initialize
-    @substitute = "?"
-  end
+module Rails
+  module Dom
+    module Testing
+      module Assertions
+        module SelectorAssertions
+          class SubstitutionContext # :nodoc:
+            def initialize
+              @substitute = "?"
+            end
 
-  def substitute!(selector, values, format_for_presentation = false)
-    selector.gsub @substitute do |match|
-      next match[0] if values.empty? || !substitutable?(values.first)
-      matcher_for(values.shift, format_for_presentation)
-    end
-  end
+            def substitute!(selector, values, format_for_presentation = false)
+              selector.gsub @substitute do |match|
+                next match[0] if values.empty? || !substitutable?(values.first)
+                matcher_for(values.shift, format_for_presentation)
+              end
+            end
 
-  def match(matches, attribute, matcher)
-    matches.find_all { |node| node[attribute] =~ Regexp.new(matcher) }
-  end
+            def match(matches, attribute, matcher)
+              matches.find_all { |node| node[attribute] =~ Regexp.new(matcher) }
+            end
 
-  private
-    def matcher_for(value, format_for_presentation)
-      # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
-      if format_for_presentation
-        value.inspect # Avoid to_s so Regexps aren't put in quotes.
-      else
-        "\"#{value}\""
+            private
+              def matcher_for(value, format_for_presentation)
+                # Nokogiri doesn't like arbitrary values without quotes, hence inspect.
+                if format_for_presentation
+                  value.inspect # Avoid to_s so Regexps aren't put in quotes.
+                else
+                  "\"#{value}\""
+                end
+              end
+
+              def substitutable?(value)
+                [ Symbol, Numeric, String, Regexp ].any? { |type| value.is_a? type }
+              end
+          end
+        end
       end
     end
-
-    def substitutable?(value)
-      [ Symbol, Numeric, String, Regexp ].any? { |type| value.is_a? type }
-    end
+  end
 end
